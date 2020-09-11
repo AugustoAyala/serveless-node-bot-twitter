d51ae384cfff98ce6e2629a71a965afdcf2a152f
augusto <ayala_815@hotmail.com>
2020-09-11 17:31:27 -0300

    second commit

--- handler.js
+++ handler.js
@@ -1,37 +1,32 @@
-const twit = require('twit');
+const twit = require("twit");
 
 const t = new twit({
-    consumer_key: "",
-    consumer_secret: "",
-    access_token: "",
-    access_token_secret: ""
+  consumer_key: "",
+  consumer_secret: "",
+  access_token: "",
+  access_token_secret: "",
 });
 
 module.exports.botTwitter = (event, context, callback) => {
-    let params = {
-        q:'#javascript',
-        result_type:'recent',
-        count:150
+  let params = {
+    q: "#javascript",
+    result_type: "recent",
+    count: 150,
+  };
+  t.get("search/tweets", params, (err, data, response) => {
+    let tweets = data.statuses;
+    if (!err) {
+      for (let tweet of tweets) {
+        let id = tweet.id_str;
+        t.post("statuses/retweet/:id", { id: id }, (err, response) => {
+          if (response) {
+            return callback(null, "It was retweeted with the" + id);
+          }
+          if (err) {
+            return callback("error", null);
+          }
+        });
+      }
     }
-    t.get('search/tweets', params,(err,data,response)=>
-    {
-        let tweets = data.statuses
-        if(!err)
-        {
-            for(let tweet of tweets)
-            {
-                let id = tweet.id_str;
-                t.post('statuses/retweet/:id', {id: id}, (err, response)=>
-                {
-                    if (response) {
-                        return callback(null, 'It was retweeted with the' + id);
-                    }
-                    if (err) {
-                        return callback('error', null)
-                    }
-                })
-                
-            }
-        }
-    })
-}
+  });
+};
